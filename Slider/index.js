bee65500ec2885c7ac74fc7488f4a0d6321157c1
SPACEta <tatyana4aris@mail.ru>
2023-10-16 00:30:15 +0300

    Add files

---
+++ Slider/index.js
@@ -0,0 +1,11 @@
+const img = document.getElementById('img');
+
+console.log(img)
+
+const left = () => {
+	img.style.backgroundImage = 'url("assets/images/Rectangle2.png")'
+}
+
+const right = () => {
+	img.style.backgroundImage = 'url("assets/images/Rectangle1.png")'
+}
